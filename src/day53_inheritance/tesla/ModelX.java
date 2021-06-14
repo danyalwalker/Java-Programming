673079b0dc12aa9587f2919f773da613ee1e194f
Daniel <danielwalker.ny@gmail.com>
2021-06-13 22:39:20 -0400

    Final Keyword

---
+++ src/day53_inheritance/tesla/ModelX.java
@@ -0,0 +1,11 @@
+package day53_inheritance.tesla;
+
+import day53_inheritance.tesla.ElectricCar;
+
+public class ModelX extends ElectricCar {
+
+    public ModelX(String make, String model, double price, int year, int range) {
+        super(make, model, price, year, range);
+    }
+}
+
