083ce867dd0d806e4ed7247ae09928fba3548fa7
Daniel <danielwalker.ny@gmail.com>
2021-06-30 03:02:27 -0400

    Abstract & Interface Practice

---
+++ src/day58_polymorphism/Etsy.java
@@ -0,0 +1,18 @@
+package day58_polymorphism;
+
+public class Etsy extends OnlineShopping implements Handmade{
+    @Override
+    public void buy() {
+
+    }
+
+    @Override
+    public void sell() {
+
+    }
+
+    @Override
+    public void creativeArt() {
+
+    }
+}
